44236768016029c85498aa2c9fc39b7a283b9178
Leonardo Nunes Moura <leonardo.nunes@soulvarejo.com>
2025-01-24 16:31:41 -0300

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import "@ui5/webcomponents/dist/Button.js";
+import "@ui5/webcomponents/dist/Label.js";
+import "@ui5/webcomponents-fiori/dist/BarcodeScannerDialog.js";
+import "@ui5/webcomponents-icons/dist/camera.js"
 
 @NgModule({
   declarations: [
@@ -12,6 +17,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
